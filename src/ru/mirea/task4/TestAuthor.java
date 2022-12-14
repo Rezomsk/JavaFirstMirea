81fe984a8f95d6e406c519e6798c07ac9b1be4fc
Рза Рзгоян <116155463+Rezomsk@users.noreply.github.com>
2022-12-14 03:05:26 +0300

    Add files via upload

---
+++ src/ru/mirea/task4/TestAuthor.java
@@ -0,0 +1,15 @@
+package ru.mirea.task4;
+
+import java.util.Scanner;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author oleg = new Author("Rezo", "Rezo@gmail.com", 'M');
+        System.out.println(Rezo);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter new email: ");
+        String email = scan.nextLine();
+        System.out.println(Rezo);
+
+    }
+}
